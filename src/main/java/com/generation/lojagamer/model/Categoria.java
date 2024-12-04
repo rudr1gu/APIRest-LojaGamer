f55e53dde4181f86d91e94dc65666ca97245b79b
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-04 11:15:32 -0300

    :sparkles: feat: add produtos relationship to Categoria model

--- src/main/java/com/generation/lojagamer/model/Categoria.java
+++ src/main/java/com/generation/lojagamer/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.lojagamer.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -26,4 +33,8 @@ public class Categoria {
     @NotNull 
     @Size(min = 5, max = 100)
     private String descricao;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("categoria")
+    private List<Produtos> produtos;
 }
